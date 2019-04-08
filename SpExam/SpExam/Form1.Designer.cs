d0381e378e9bd9924bc1169e8ae76ea394cf6f87
Duysenaliyev Alibi <40587377+realibi@users.noreply.github.com>
2019-04-08 23:01:27 +0600

    Add files via upload

---
+++ SpExam/SpExam/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SpExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPathToSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPathToCopy = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPathToDictionary = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnPathToSearch = new System.Windows.Forms.Button();
+            this.btnPathToCopy = new System.Windows.Forms.Button();
+            this.btnPathToDictionary = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPathToSearch
+            // 
+            this.txtPathToSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPathToSearch.Location = new System.Drawing.Point(124, 18);
+            this.txtPathToSearch.Name = "txtPathToSearch";
+            this.txtPathToSearch.Size = new System.Drawing.Size(203, 20);
+            this.txtPathToSearch.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Где искать";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(199, 313);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Начать";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Куда копировать";
+            // 
+            // txtPathToCopy
+            // 
+            this.txtPathToCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPathToCopy.Location = new System.Drawing.Point(124, 56);
+            this.txtPathToCopy.Name = "txtPathToCopy";
+            this.txtPathToCopy.Size = new System.Drawing.Size(203, 20);
+            this.txtPathToCopy.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Словарь";
+            // 
+            // txtPathToDictionary
+            // 
+            this.txtPathToDictionary.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPathToDictionary.Location = new System.Drawing.Point(124, 92);
+            this.txtPathToDictionary.Name = "txtPathToDictionary";
+            this.txtPathToDictionary.Size = new System.Drawing.Size(203, 20);
+            this.txtPathToDictionary.TabIndex = 5;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(294, 313);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Выйти";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnPathToSearch
+            // 
+            this.btnPathToSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPathToSearch.Location = new System.Drawing.Point(333, 17);
+            this.btnPathToSearch.Name = "btnPathToSearch";
+            this.btnPathToSearch.Size = new System.Drawing.Size(38, 21);
+            this.btnPathToSearch.TabIndex = 9;
+            this.btnPathToSearch.Text = "...";
+            this.btnPathToSearch.UseVisualStyleBackColor = true;
+            this.btnPathToSearch.Click += new System.EventHandler(this.btnPathToSearch_Click);
+            // 
+            // btnPathToCopy
+            // 
+            this.btnPathToCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPathToCopy.Location = new System.Drawing.Point(333, 55);
+            this.btnPathToCopy.Name = "btnPathToCopy";
+            this.btnPathToCopy.Size = new System.Drawing.Size(38, 21);
+            this.btnPathToCopy.TabIndex = 10;
+            this.btnPathToCopy.Text = "...";
+            this.btnPathToCopy.UseVisualStyleBackColor = true;
+            this.btnPathToCopy.Click += new System.EventHandler(this.btnPathToCopy_Click);
+            // 
+            // btnPathToDictionary
+            // 
+            this.btnPathToDictionary.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPathToDictionary.Location = new System.Drawing.Point(333, 91);
+            this.btnPathToDictionary.Name = "btnPathToDictionary";
+            this.btnPathToDictionary.Size = new System.Drawing.Size(38, 21);
+            this.btnPathToDictionary.TabIndex = 11;
+            this.btnPathToDictionary.Text = "...";
+            this.btnPathToDictionary.UseVisualStyleBackColor = true;
+            this.btnPathToDictionary.Click += new System.EventHandler(this.btnPathToDictionary_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 270);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(356, 23);
+            this.progressBar.TabIndex = 12;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(15, 156);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(353, 98);
+            this.txtLog.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Результат";
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPause.Location = new System.Drawing.Point(15, 313);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 16;
+            this.btnPause.Text = "Пауза";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResume.Location = new System.Drawing.Point(106, 313);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 17;
+            this.btnResume.Text = "Продолж.";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 346);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnPathToDictionary);
+            this.Controls.Add(this.btnPathToCopy);
+            this.Controls.Add(this.btnPathToSearch);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPathToDictionary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPathToCopy);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPathToSearch);
+            this.Name = "Form1";
+            this.Text = "Цензура";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPathToSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPathToCopy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPathToDictionary;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnPathToSearch;
+        private System.Windows.Forms.Button btnPathToCopy;
+        private System.Windows.Forms.Button btnPathToDictionary;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnResume;
+    }
+}
+
---
+++ SpExam/SpExam/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SpExam
+{
+    public partial class Form1 : Form
+    {
+        Thread thSearch = null;
+        EventWaitHandle evn = null;
+        List<string> reportInfo = new List<string>();
+
+        string[] dictionary = null;
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Словарь - это файл с плохими словами, которые нужно найти при сканировании. Можете сформировать такой файл сами - просто вводите туда слова через пробел, а потом укажите на этот файл. Далее, нужно указать на директорию с файлами для сканирования, в нем могут содержаться файлы и другие папки. Путь копирования - это путь, куда будут копироваться файлы, в которых нашлись плохие слова. Так же по этому пути будет находиться файл отчета \"Report.txt\"", "Инструкция пользователя", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            txtLog.ScrollBars = ScrollBars.Vertical;
+        }
+
+        private void InitDictionary()
+        {
+            List<string> lines = new List<string>();
+            using (StreamReader reader = new StreamReader(txtPathToDictionary.Text))
+            {
+                while (!reader.EndOfStream) lines.Add(reader.ReadLine());
+            }
+            foreach(var row in lines)
+            {
+                dictionary = row.Split(' ');
+            }
+        }
+
+        private void btnPathToSearch_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtPathToSearch.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnPathToCopy_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtPathToCopy.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnPathToDictionary_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtPathToDictionary.Text = ofd.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            InitDictionary();
+
+            thSearch = new Thread(SearchRoutine);
+            thSearch.IsBackground = true;
+            thSearch.Start();
+        }
+
+        private void SearchRoutine()
+        {   
+            DirectoryInfo directoryInfo = new DirectoryInfo(txtPathToSearch.Text);
+
+            DirectoryInfo[] subDirectories = directoryInfo.GetDirectories();
+
+            //replaceWords(directoryInfo, dictionary);
+
+            replaceWords(directoryInfo, dictionary);
+                
+
+            Report(reportInfo);
+
+            DialogResult res = MessageBox.Show("Хотите открыть файл отчета?", "Конец работы программы", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (res == DialogResult.Yes) Process.Start(txtPathToCopy.Text + "\\Report.txt");
+            else Application.Exit();
+        }
+
+        private void replaceWords(DirectoryInfo di, string[] dictionary)
+        {
+            //reportInfo = new List<string>();
+
+            FileInfo[] files = di.GetFiles("*.txt");
+
+            progressBar.Invoke(new Action<int>(
+            (x) => 
+            {
+                    progressBar.Maximum = x;
+                    progressBar.Update();
+             }), files.Length);
+
+            for (int i = 1; i <= files.Length; i++)
+            {
+                string textFromFile = string.Empty;
+                string[] wordsFromFile = null;
+                int swearCount = 0;
+
+                using (StreamReader reader = File.OpenText(files[i-1].FullName))
+                {
+                    textFromFile = reader.ReadToEnd();
+                }
+
+                wordsFromFile = textFromFile.Split(' ');
+
+                foreach(var swear in dictionary)
+                {
+                    foreach(var word in wordsFromFile)
+                    {
+                        if(swear == word)
+                        {
+                            swearCount++;
+                            textFromFile = textFromFile.Replace(word, "*******");
+                            reportInfo.Add("Путь к файлу: " + files[i-1].FullName
+                                + " | Размер файла: " + files[i-1].Length
+                                + " | Замененное слово: " + swear + ' ');
+
+                            using (StreamWriter write = new StreamWriter(txtPathToCopy.Text + "\\" + files[i - 1].Name, false))
+                            {
+                                write.Write(textFromFile);
+                            }
+                        }
+                    }   
+                }
+
+                progressBar.Invoke(new Action<int>((x)=> {
+                    progressBar.Value = x;
+                    progressBar.Update();
+                }), i);
+            }
+
+            if (di.GetDirectories().Length != 0)
+            {
+                DirectoryInfo[] subDirs = di.GetDirectories();
+
+                foreach(var item in subDirs) replaceWords(item, dictionary);
+            }
+        }
+
+        private void Report(List<string> reportInfo)
+        {
+            string textFromReport = string.Empty;
+            string[] wordsFromReport = null;
+            SortedDictionary<int, string> raiting = new SortedDictionary<int, string>();
+
+            using (StreamWriter write = new StreamWriter(txtPathToCopy.Text + "\\Report.txt", false))
+            {
+                foreach (var item in reportInfo)
+                {
+                    write.WriteLine(item);
+                }
+            }
+
+            using (StreamReader read = new StreamReader(txtPathToCopy.Text + "\\Report.txt", false))
+            {
+                textFromReport = read.ReadToEnd();
+            }
+
+            wordsFromReport = textFromReport.Split(' ');
+
+            using (StreamWriter write = new StreamWriter(txtPathToCopy.Text + "\\Report.txt", false))
+            {
+                int top = 1;
+
+                write.Write("Топ замененных плохих слов:\r\n");
+
+                txtLog.Invoke(new Action<string>((x) =>
+                {
+                    txtLog.Text += x;
+                    txtLog.Update();
+                }), "Данный отчет так же записан в соответствующий файл в указанной папке для копирования.\r\n\r\n");
+
+                txtLog.Invoke(new Action<string>((x) =>
+                {
+                    txtLog.Text += x;
+                    txtLog.Update();
+                }), "Топ замененных плохих слов:\r\n");
+
+                foreach (var item in dictionary)
+                {
+                    //write.WriteLine($"Слово {item} встречалось {Raiting(item, wordsFromReport)} раза!");
+                    if(!raiting.ContainsKey(Raiting(item, wordsFromReport)))
+                    {
+                        raiting.Add(Raiting(item, wordsFromReport), item);
+                    }
+                }
+
+                for (int index = raiting.Count; index > 0; index--)
+                {
+                    var item = raiting.ElementAt(index-1);
+                    var itemKey = item.Key;
+                    var itemValue = item.Value;
+                    write.WriteLine($"{top}) Слово {item.Value} встречалось {item.Key} раза!");
+
+                    txtLog.Invoke(new Action<string>((x)=> 
+                    {
+                        txtLog.Text += x;
+                        txtLog.Update();
+                    }), $"{top}) Слово {item.Value} встречалось {item.Key} раза!" + "\r\n");
+                    top++;
+                }
+
+                write.Write("\r\n\r\n" + textFromReport);
+
+                txtLog.Invoke(new Action<string>((x) =>
+                {
+                    txtLog.Text += x;
+                    txtLog.Update();
+                }), "\r\n" + textFromReport);
+            }
+        }
+
+        private int Raiting(string swear, string[] wordsFromFile)
+        {
+            int count = 0;
+
+            foreach(var item in wordsFromFile)
+            {
+                if (item == swear) count++;
+            }
+
+            return count;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thSearch.Suspend();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thSearch.Resume();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
